9fc8656b9b92fa4ddf1fe16b171b2815b4534282
Peter <peter@zoneofdanger.com>
2020-07-27 14:23:54 -0700

    merge all eslint rules into a single one for typescript
    remove unused eslint rules

--- eslint/index.js
+++ eslint/index.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['airbnb-base', require.resolve('./rules/default'), 'prettier'],
+  extends: ['airbnb-base', require.resolve('./rules/typescript'), 'prettier'],
   rules: {},
   ignorePatterns: ['node_modules/', 'dist', 'package.json'],
 };
