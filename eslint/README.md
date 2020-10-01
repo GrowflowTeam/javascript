9143456a5977f5dcb2a000c7cf81c373a604c412
Chad Lee <chad@growflow.com>
2020-10-01 10:11:09 -0500

    chore: add more linting rules and plugins
    Inspired from here: https://iamturns.com/typescript-babel/

--- eslint/README.md
+++ eslint/README.md
@@ -12,29 +12,15 @@ yarn add @growflow/eslint-config eslint prettier -D
 
 > Note: that you'll need a `.npmrc` file with Growflow's `NPM_TOKEN`.
 
-You'll need to create a `.eslintrc.json` or `.eslintrc` file with content similar to the following:
+You'll need to create a `.eslintrc.js` file with content similar to the following:
 
-```json
-{
-  "extends": ["@growflow/eslint-config"],
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "project": "./tsconfig.json"
+```js
+module.exports = {
+  extends: ['@growflow'],
+  parserOptions: {
+    project: 'tsconfig.json',
   },
-  "root": true,
-  "ignorePatterns": [
-    "config/*",
-    "node_modules",
-    "dist",
-    "package.json",
-    "__GENERATED__",
-    "cypress",
-    "jest",
-    "__TESTS__",
-    "*.stories.js",
-    "*.spec.js"
-  ]
-}
+};
 ```
 
 ### Usage with Prettier
--- eslint/index.js
+++ eslint/index.js
@@ -1,13 +1,35 @@
 module.exports = {
+  plugins: [
+    '@typescript-eslint',
+    'eslint-comments',
+    'promise',
+    'unicorn',
+    'simple-import-sort',
+    'monorepo',
+  ],
   extends: [
-    'airbnb',
+    'airbnb-typescript',
     'airbnb/hooks',
-    '@growflow/eslint-config/rules/typescript',
+    'plugin:eslint-comments/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:promise/recommended',
+    'plugin:unicorn/recommended',
+    'plugin:monorepo/recommended',
     'plugin:prettier/recommended',
     'prettier/react',
     'prettier/@typescript-eslint',
   ],
+  env: {
+    node: true,
+    browser: true,
+  },
   rules: {
+    // sort imports
+    'simple-import-sort/sort': 'error',
+    'sort-imports': 'off',
+    'import/order': 'off',
+
     // we use typescript; ain't need no prop-types
     'react/prop-types': 'off',
     'react/require-default-props': 'off',
@@ -18,11 +40,50 @@ module.exports = {
     // it's not 2018 anymore, let's put our JSX where we want it
     'react/jsx-filename-extension': 'off',
 
-    // this shit is just broken when using with React/TS for some reason
-    'no-use-before-define': 'off',
-
     // I'm not sure what issue this one is trying to prevent. it's fine; use those unescaped entities
     'react/no-unescaped-entities': 'off',
+
+    'no-use-before-define': [
+      'error',
+      { functions: false, classes: true, variables: true },
+    ],
+
+    // Makes no sense to allow type inference for expression parameters, but require typing the response
+    '@typescript-eslint/explicit-function-return-type': [
+      'warn',
+      { allowExpressions: true, allowTypedFunctionExpressions: true },
+    ],
+
+    '@typescript-eslint/no-use-before-define': [
+      'error',
+      { functions: false, classes: true, variables: true, typedefs: true },
+    ],
+
+    // https://stackoverflow.com/a/63833015/316108
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars-experimental': 'error',
+
+    // it's fine, just relax
+    'no-param-reassign': 'off',
+
+    // just relax, unicorn
+    'unicorn/prevent-abbreviations': 'off',
+    'unicorn/prefer-query-selector': 'off',
+    'unicorn/no-fn-reference-in-iterator': 'off',
   },
-  ignorePatterns: ['node_modules/', 'dist', 'package.json'],
+  ignorePatterns: ['node_modules/', 'dist', 'package.json', '__generated__'],
+  overrides: [
+    {
+      files: ['*.js', '*.jsx'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-unsafe-member-access': 'off',
+        '@typescript-eslint/no-unsafe-assignment': 'off',
+        '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off',
+      },
+    },
+  ],
 };
