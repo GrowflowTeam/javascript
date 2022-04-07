ceab62daa3c9d52c77650d383aeca7e1bd41725d
Chad Lee <chad@growflow.com>
2022-04-07 08:18:10 -0500

    🐛 fix: jest rule name changes

--- packages/eslint/jest.js
+++ packages/eslint/jest.js
@@ -6,17 +6,11 @@ module.exports = {
   },
   rules: {
     'jest/consistent-test-it': 'error',
-    'jest/lowercase-name': 'error',
     'jest/no-test-return-statement': 'error',
     'jest/prefer-hooks-on-top': 'error',
-
-    'jest/no-if': 'warn',
+    'jest/prefer-lowercase-title': 'error',
     'jest/prefer-spy-on': 'warn',
     'jest/prefer-todo': 'warn',
-
-    // allow these rules for mocking purposes
-    'global-require': 'off',
-    '@typescript-eslint/no-var-requires': 'off',
 
     // be more liberal with typescript warnings/errors since we are writing tests anyway...
     '@typescript-eslint/no-unsafe-assignment': 'off',
